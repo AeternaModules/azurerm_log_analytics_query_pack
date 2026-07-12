9c53a1002974596e384a892aaf45e1bab846ef9f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "log_analytics_query_packs" {
-  description = "All log_analytics_query_pack resources"
-  value       = azurerm_log_analytics_query_pack.log_analytics_query_packs
-}
 output "log_analytics_query_packs_location" {
-  description = "List of location values across all log_analytics_query_packs"
-  value       = [for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : v.location]
+  description = "Map of location values across all log_analytics_query_packs, keyed the same as var.log_analytics_query_packs"
+  value       = { for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : k => v.location }
 }
 output "log_analytics_query_packs_name" {
-  description = "List of name values across all log_analytics_query_packs"
-  value       = [for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : v.name]
+  description = "Map of name values across all log_analytics_query_packs, keyed the same as var.log_analytics_query_packs"
+  value       = { for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : k => v.name }
 }
 output "log_analytics_query_packs_resource_group_name" {
-  description = "List of resource_group_name values across all log_analytics_query_packs"
-  value       = [for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : v.resource_group_name]
+  description = "Map of resource_group_name values across all log_analytics_query_packs, keyed the same as var.log_analytics_query_packs"
+  value       = { for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : k => v.resource_group_name }
 }
 output "log_analytics_query_packs_tags" {
-  description = "List of tags values across all log_analytics_query_packs"
-  value       = [for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : v.tags]
+  description = "Map of tags values across all log_analytics_query_packs, keyed the same as var.log_analytics_query_packs"
+  value       = { for k, v in azurerm_log_analytics_query_pack.log_analytics_query_packs : k => v.tags }
 }
 
